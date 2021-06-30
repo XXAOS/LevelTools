208b88890bcbf5951c25e97d12f62828f2565e56
haydensaidler <saidlerfamily@gmail.com>
2021-06-30 12:28:49 -0700

    Updated JSON message API

--- src/main/java/me/Hayden/LevelTools/Command.java
+++ src/main/java/me/Hayden/LevelTools/Command.java
@@ -1,24 +1,26 @@
 package me.Hayden.LevelTools;
 
-import mkremins.fanciful.FancyMessage;
+import me.rayzr522.jsonmessage.JSONMessage;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
 import static org.bukkit.ChatColor.*;
 
 public class Command implements CommandExecutor {
     static String chat(String s) {
         return ChatColor.translateAlternateColorCodes('&', s);
     }
-    static FancyMessage about() {
-        return new FancyMessage("   [VERSION]")
+    static JSONMessage about() {
+        return JSONMessage.create("   [VERSION]")
                 .color(GREEN)
                 .style(BOLD)
                 .tooltip(chat("&7Version &2" + Main.plugin.getDescription().getVersion()))
                 .then("            [HOOKS]")
                 .color(RED)
                 .style(BOLD)
-                .tooltip(chat("&cAutoSell: " + Main.autosell_hook), chat("&eTokenEnchant " + Main.tokenenchant_hook), chat( "&bCrazyEnchant: " + Main.crazyenchant_hook));
+                .tooltip(chat("&cAutoSell: " + Main.autosell_hook)+"\n"+chat("&eTokenEnchant " + Main.tokenenchant_hook)+"\n"+ chat( "&bCrazyEnchant: " + Main.crazyenchant_hook));
     }
     public boolean onCommand(CommandSender sender, org.bukkit.command.Command cmd, String s, String[] args) {
 
@@ -29,7 +31,7 @@ public class Command implements CommandExecutor {
         }
 
         sender.sendMessage(chat("                 &a&lLevelTools+ "));
-        about().send(sender);
+        about().send((Player) sender);
         return false;
     }
 }
